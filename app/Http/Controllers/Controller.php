e18081430687e7bfd9d14bad7a7db3eb8af0c43e
azzahraayy <azzahraafidahyusfa@gmail.com>
2026-06-26 16:32:40 +0700

    ini terbaru banget sih

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use OpenApi\Attributes as OAT;
 
 #[OAT\Info(
     version: "1.0.0",
-    description: "API untuk mengelola review produk",
+    description: "API untuk mengelola review produk pelanggan",
     title: "Customer Review Service API"
 )]
 #[OAT\SecurityScheme(
@@ -15,6 +15,7 @@ use OpenApi\Attributes as OAT;
     name: "X-IAE-KEY",
     in: "header"
 )]
+#[OAT\Server(url: "http://localhost:8000", description: "Local Docker Server")]
 abstract class Controller
 {
     //
--- app/Http/Controllers/SwaggerInfo.php
+++ app/Http/Controllers/SwaggerInfo.php
@@ -2,21 +2,5 @@
 
 namespace App\Http\Controllers;
 
-use OpenApi\Attributes as OAT;
-
-#[OAT\OpenApi(
-    openapi: '3.0.0',
-    info: new OAT\Info(
-        title: 'Customer Review Service API',
-        version: '1.0.0',
-        description: 'API untuk mengelola review produk pelanggan'
-    )
-)]
-#[OAT\SecurityScheme(
-    securityScheme: 'ApiKeyAuth',
-    type: 'apiKey',
-    in: 'header',
-    name: 'X-IAE-KEY'
-)]
-#[OAT\Server(url: 'http://localhost:8000', description: 'Local Docker Server')]
+// OpenAPI info is declared in Controller.php to avoid duplicate @OA\Info errors
 class SwaggerInfo {}
